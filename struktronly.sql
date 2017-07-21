afc7b4a100c63f7acea5db2f9687580385b61153
Melvandito Simanjuntak <oktavianmelvandito@gmail.com>
2017-07-21 09:42:37 +0700

    Add files via upload

---
+++ struktronly.sql
@@ -0,0 +1,320 @@
+/*
+SQLyog Community v12.4.3 (64 bit)
+MySQL - 10.1.16-MariaDB : Database - sihimastitest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sihimastitest` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sihimastitest`;
+
+/*Table structure for table `announcement` */
+
+DROP TABLE IF EXISTS `announcement`;
+
+CREATE TABLE `announcement` (
+  `announcement_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `body` text NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`announcement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `announcement_file` */
+
+DROP TABLE IF EXISTS `announcement_file`;
+
+CREATE TABLE `announcement_file` (
+  `announcement_file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `announcement_id` int(11) NOT NULL,
+  PRIMARY KEY (`announcement_file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `auth_rule` */
+
+DROP TABLE IF EXISTS `auth_rule`;
+
+CREATE TABLE `auth_rule` (
+  `name` varchar(64) NOT NULL,
+  `data` text,
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(32) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(32) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `deleted_at` datetime NOT NULL,
+  `deleted_by` varchar(32) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `board` */
+
+DROP TABLE IF EXISTS `board`;
+
+CREATE TABLE `board` (
+  `board_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `desc` text NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`board_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `board_activity` */
+
+DROP TABLE IF EXISTS `board_activity`;
+
+CREATE TABLE `board_activity` (
+  `board_activity_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `desc` text NOT NULL,
+  `link` tinytext,
+  `access_of_time` datetime NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `board_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`board_activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `code` varchar(32) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `finance` */
+
+DROP TABLE IF EXISTS `finance`;
+
+CREATE TABLE `finance` (
+  `finance_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_time` datetime NOT NULL,
+  `desc` text NOT NULL,
+  `type` char(1) NOT NULL,
+  `nominal` double NOT NULL,
+  `last_balance` double NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`finance_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `g_gallery` */
+
+DROP TABLE IF EXISTS `g_gallery`;
+
+CREATE TABLE `g_gallery` (
+  `gallery_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(32) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `deleted_at` datetime NOT NULL,
+  `deleted_by` varchar(32) NOT NULL,
+  PRIMARY KEY (`gallery_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `g_photo` */
+
+DROP TABLE IF EXISTS `g_photo`;
+
+CREATE TABLE `g_photo` (
+  `photo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(32) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(32) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `deleted_at` datetime NOT NULL,
+  `deleted_by` varchar(32) NOT NULL,
+  PRIMARY KEY (`photo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `member` */
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nim` varchar(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `generation` int(11) NOT NULL,
+  `address` text NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `religion` varchar(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `migration` */
+
+DROP TABLE IF EXISTS `migration`;
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `rule` */
+
+DROP TABLE IF EXISTS `rule`;
+
+CREATE TABLE `rule` (
+  `rule_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `desc` text NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`rule_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `tutorial` */
+
+DROP TABLE IF EXISTS `tutorial`;
+
+CREATE TABLE `tutorial` (
+  `tutorial_id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic` varchar(200) NOT NULL,
+  `tutor` varchar(200) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `desc` text NOT NULL,
+  `access_of_time` datetime NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  PRIMARY KEY (`tutorial_id`),
+  KEY `id_matakuliah` (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `tutorial_file` */
+
+DROP TABLE IF EXISTS `tutorial_file`;
+
+CREATE TABLE `tutorial_file` (
+  `tutorial_file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` varchar(32) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(32) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` varchar(32) DEFAULT NULL,
+  `tutorial_id` int(11) NOT NULL,
+  PRIMARY KEY (`tutorial_file_id`),
+  KEY `id_tutorial` (`tutorial_id`),
+  KEY `id_tutorial_2` (`tutorial_id`),
+  KEY `id_tutorial_3` (`tutorial_id`),
+  KEY `id_tutorial_4` (`tutorial_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `deleted_at` datetime NOT NULL,
+  `deleted_by` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
